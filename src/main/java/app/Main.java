1abb9b7da64ca975f24dadc56042efc1724bf5d0
vuk <vbozovic14@yahoo.com>
2018-11-29 18:37:51 +0100

    First prototype done

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,24 @@
 package app;
 
+import app.server.Server;
+
 public class Main {
 
     public static void main(String[] args){
 
+        Server s = new Server(8080,"localhost");
 
+        Thread t = new Thread(s);
+
+
+        String addr = args[0];
+        s.establish(addr);
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
