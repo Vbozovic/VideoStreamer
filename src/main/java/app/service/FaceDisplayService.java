be764ebff307161d8812eb2d9ebe4cc8f1437fa0
vukbozovic <vuk.bozovic@p3-group.com>
2019-02-20 19:04:08 +0100

    Refactor FaceDisplayService

--- src/main/java/app/service/FaceDisplayService.java
+++ src/main/java/app/service/FaceDisplayService.java
@@ -26,26 +26,14 @@ public class FaceDisplayService {
         int counter = 0;
         //Add face Panel to displayTo
         for (PersonPanel pan : panels) {
-            int UX,UY,LX,LY;
-            UX = faces.get(counter).ULX();
-            UY = faces.get(counter).ULY();
-            LX = faces.get(counter).LRX();
-            LY = faces.get(counter).LRY();
-
-            ImagePanel imPan = new ImagePanel(image.getSubimage(UX,UY,LX-UX,LY-UY),10,10);
+            ImagePanel imPan = panelCrop(image,counter,faces);
             pan = new PersonPanel(imPan,faces.get(counter));
             pan.repaint();
             counter++;
         }
 
         for (; counter < faces.size(); counter++) {
-            int UX,UY,LX,LY;
-            UX = faces.get(counter).ULX();
-            UY = faces.get(counter).ULY();
-            LX = faces.get(counter).LRX();
-            LY = faces.get(counter).LRY();
-
-            ImagePanel imPan = new ImagePanel(image.getSubimage(UX,UY,LX-UX,LY-UY),10,10);
+            ImagePanel imPan = panelCrop(image,counter,faces);
             PersonPanel ppan = new PersonPanel(imPan,faces.get(counter));
             displayTo.add(ppan);
             ppan.repaint();
@@ -55,5 +43,15 @@ public class FaceDisplayService {
         displayTo.repaint();
     }
 
+    private ImagePanel panelCrop(BufferedImage image,int counter,List<FaceDto> faces){
+        int UX,UY,LX,LY;
+        UX = faces.get(counter).ULX();
+        UY = faces.get(counter).ULY();
+        LX = faces.get(counter).LRX();
+        LY = faces.get(counter).LRY();
+
+        ImagePanel imPan = new ImagePanel(image.getSubimage(UX,UY,LX-UX,LY-UY),10,10);
+        return imPan;
+    }
 
 }
