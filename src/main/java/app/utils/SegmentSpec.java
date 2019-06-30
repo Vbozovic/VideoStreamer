ee045111df759e4210ba834d968b0789f553fa39
Vbozovic <vbozovic14@yahoo.com>
2019-06-30 16:22:02 +0200

    Read from file channel

---
+++ src/main/java/app/utils/SegmentSpec.java
@@ -0,0 +1,12 @@
+package app.utils;
+
+public class SegmentSpec {
+
+    public int frames;
+    public String file;
+
+    public SegmentSpec(int frames, String file) {
+        this.frames = frames;
+        this.file = file;
+    }
+}
